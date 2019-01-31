c98c976255d6ab03d650f1a8e56ad9ba2196aa7c
CHAMPLAIN\claire.yeash <claire.yeash@mymail.champlain.edu>
2019-01-31 18:22:10 -0500

    Added the UI, scoring, timer, and working on player health

---
+++ Prototype3/Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// This script is located under the GameManager object and is used to...
+public class GameManager : MonoBehaviour
+{
+    //timer
+    public float timer;
+    public TextMeshProUGUI timerText;
+
+    //score
+    public float score;
+    public TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 60f; // in seconds; sets the countdown to the selected game length
+        Time.timeScale = 1;
+
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // updates the timer in the UI
+        timer -= Time.deltaTime;
+        timerText.text = timer.ToString("f0");
+
+        // tests to see if the score is being added
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            AddPoints(1);
+        }
+    }
+
+    // called each time the player earns points
+    public void AddPoints(int pointsAdded)
+    {
+        score += pointsAdded;
+        scoreText.text = score.ToString();
+    }
+}
---
+++ Prototype3/Assets/PlayerHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is on the player object and is used to keep track of the player's health
+public class PlayerHealth : MonoBehaviour
+{
+    public int playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Damage(1);
+        }
+
+        if(collision.gameObject.tag == "Light")
+        {
+            PickUpLightBulb();
+            Destroy(gameObject);
+        }
+    }
+
+    // accessor for the player health
+    public int GetHealth()
+    {
+        return playerHealth;
+    }
+
+    // takes the damage based on the enemy
+    void Damage(int enemyStrength)
+    {
+        playerHealth -= enemyStrength;
+    }
+
+    // adds one to the player's health when they pick up a lightbulb
+    public void PickUpLightBulb()
+    {
+        playerHealth += 1;
+    }
+}
