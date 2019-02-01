3c2d9f66a63dd27aaa145383547300c01c44689f
ZachP <zphillips146@gmail.com>
2019-01-31 21:41:50 -0500

    Added the camera movement!

--- Prototype3/Assets/Scripts/MovementScript.cs
+++ Prototype3/Assets/Scripts/MovementScript.cs
@@ -12,9 +12,16 @@ public class MovementScript : MonoBehaviour
     private Player player;
     public int playerId = 0;
 
+    public Transform cameraFocus;
+    public float rootLow;
+    public float rootHigh;
+    public float cameraPitchSpeed = 1;
+    public float cameraRotateSpeed = 1;
+
     bool isGrounded = false;
     Rigidbody rb;
     Vector3 move;
+    Vector2 look;
 
     // Start is called before the first frame update
     void Start()
@@ -27,18 +34,39 @@ public class MovementScript : MonoBehaviour
     void Update()
     {
         Movement();
+        CameraMovement();
+    }
 
+    void CameraMovement()
+    {
+        look.y = player.GetAxis("LookVertical");
+        look.x = player.GetAxis("LookHorizontal");
+
+        //Up and down movement
+        if (look.y > 0.2 && cameraFocus.transform.localPosition.y < rootHigh)
+        {
+            cameraFocus.transform.Translate(Vector3.up * cameraPitchSpeed);
+        }
+        else if (look.y < -0.2 && cameraFocus.transform.localPosition.y > rootLow)
+        {
+            cameraFocus.transform.Translate(Vector3.down * cameraPitchSpeed);            
+        }
+
+        //Right and left movement
+        gameObject.transform.Rotate(Vector3.up * look.x * cameraRotateSpeed);
+
+        Camera.main.transform.LookAt(cameraFocus);
     }
 
     void Movement()
     {
 
         move.y = player.GetAxis("Move Vertical");
-        move.x = player.GetAxis("Move Horizontal");
+        //move.x = player.GetAxis("Move Horizontal");
 
         if (isGrounded && player.GetButton("Forward"))
         {
-            move = new Vector3(move.x, 0.0f, move.y);
+            move = new Vector3(0, 0, move.y);
             rb.AddForce(move * speed);
             rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
             isGrounded = false;
