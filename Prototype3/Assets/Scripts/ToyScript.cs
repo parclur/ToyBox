09cc9d0db6454d2662f18d010424a9c2a7ecc2dc
Kelly Herstine <kelly.herstine@mymail.champlain.edu>
2019-02-01 17:04:11 -0500

    added enemy ai

--- Prototype3/Assets/Scripts/ToyScript.cs
+++ Prototype3/Assets/Scripts/ToyScript.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class ToyScript : MonoBehaviour
 {
     private float push = 5;
+    private float speed = 4;
+    private GameObject player;
+    float minDistance = 5;
+    float maxDistance = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player1");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
+        //super simple movement would fix later
+        transform.LookAt(player.transform);
+        if(Vector3.Distance(transform.position, player.transform.position) >= minDistance)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
         
     }
+
+    void Seek()
+    {
+        //Vector3 enemyDirection = target.transform.position - transform.position;
+        //enemyDirection.z = 0;
+        //float distance = enemyDirection.magnitude;
+        //float deecelelrationFactor = distance / deecelerationFactor;
+        //float speed = moveSpeed * deecelelrationFactor;
+
+        //Vector3 moveVector = enemyDirection.normalized * Time.deltaTime * speed;
+        //transform.position += moveVector;
+        //rb.velocity *= moveVector * moveSpeed * Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Player1")
